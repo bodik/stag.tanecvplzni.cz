4e1f8a60026ff7a368cb20c69a30f3d151e0a1aa
root <root@tester.gc-system.cz>
2017-07-25 00:15:25 +0200

    create test course for manual testing on db creating

--- bin/make_db.sh
+++ bin/make_db.sh
@@ -1,2 +1,4 @@
 php bin/console doctrine:schema:drop --force
 php bin/console doctrine:schema:create
+
+mysql -NBe 'insert into course (name,description,teacher,place,capacity,pair,price_single,price_pair,lessons) values ("kurz init", "init db", "make db", "db", 3, 0, 4, 5, "a:2:{i:0;s:16:\"2017-01-01T12:00\";i:1;s:16:\"2017-01-03T12:00\";}")' stagtvp
