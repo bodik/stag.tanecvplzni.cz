406783b3f5e7043e6e6c86e3fd139e5734b630d5
root <root@tester.gc-system.cz>
2017-08-01 14:56:03 +0200

    dev comit

---
+++ src/StagBundle/Tests/StagWebTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace StagBundle\Tests;
+
+#use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+#class StagWebTestCase extends WebTestCase {
+#}
+	
+use GuserBundle\Tests\GuserWebTestCase;
+class StagWebTestCase extends GuserWebTestCase {
+}
+
+?>
