9b301bf2b7d7ec0e839c7b99d8a6c97fe2c223d6
root <root@tester.gc-system.cz>
2017-08-10 13:47:55 +0200

    markdown preps

--- src/StagBundle/bin/make_db.sh
+++ src/StagBundle/bin/make_db.sh
@@ -13,8 +13,29 @@ done
 # course data
 TEXT1="Vase prihlaska byla prijata."
 TEXT2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+TEXT3="Zveme vás na kurz bachaty 4 pro pokročilé
 
-mysql -NBe "insert into course (name,level,description,lecturer,place,pair,price_single,price_pair,color,appl_email_text,picture_ref_id) values ('SALSA 1', 'zacatecnici', '${TEXT2}', 'Strejda', 'Masala Ghar', 1, 100, 200, '#527dce', '${TEXT1}', 2)" $DATABASE
+Kurz je určen pro ty, kteří navštěvovali náš kurz bachata 3 nebo ty, kteří již bachatu tančí a chtějí se v ní posunout dále. Bachata je romantický, pomalý tanec, který se často hraje na salsa tančírnách.Čekají na vás efektní figury, muzikalita, footwork a lehké padačky.
+
+Bachatu milujeme a rádi bychom tuto vášeň s vámi sdíleli.
+
+Lektoři: Jakub Peca, Jana Kučerová
+Kdy - pondělí od 11.9 - 13.11.2017 od 20.30-21.30(10 lekcí po 60 minutách)
+Kde - taneční sál v indické restauraci Masala Ghar, nám. Republiky 21, Plzeň, 2.patro
+Cena: 1000,-/osobu
+
+Hlásit se můžete i bez partnera, do určité míry přijímáme přhlášky od samotných dam. Snažíme se, aby poměr pánů a dam byl na kurzu vyrovnaný.
+
+Přihlášky na mail: jana.kucerova@tanecvplzni.cz nebo do zpráv na FB. platba před první lekcí.
+
+O lektorovi
+
+Jakub Peca - tanci se věnuje od svých 15 let, závodně tančil standard a latinu. Později přešel k salse a bachatě, které začal před 5 lety vyučovat v Mostě pod TŠ Kamily Hlavačikové.
+Loni skončil na 2.místě v bachata Jack and Jill, profi třídě lektoři. Mezi jeho oblíbené lektory bachaty, u kterých měl možnost se učit, patří například Daniel y Desireé.
+Jana Kučerová - je zakladatelkou Tance v Plzni, z. s., tančí od 9 let. Vyučuje salsu 5 let a bachatu druhým rokem. Neustále se snaží zdokonalovat u českých i zahraničních lektorů.
+V letošním roce se umístili na 4. místě v Mistrovství ČR v bachatě, vystupují na akcích po celé ČR."
+
+mysql -NBe "insert into course (name,level,description,lecturer,place,pair,price_single,price_pair,color,appl_email_text,picture_ref_id) values ('SALSA 1', 'zacatecnici', '${TEXT3}', 'Strejda', 'Masala Ghar', 1, 100, 200, '#527dce', '${TEXT1}', 2)" $DATABASE
 mysql -NBe "insert into course (name,level,description,lecturer,place,pair,price_single,price_pair,color,appl_email_text,picture_ref_id) values ('BACHATA 2', 'pokrocily', '${TEXT2}', 'Mamka', 'Salon Rounda', 1, 300, 400, '#a874cc', '${TEXT1}', 1)" $DATABASE
 mysql -NBe "insert into course (name,level,description,lecturer,place,pair,price_single,price_pair,color,appl_email_text,picture_ref_id) values ('WORKOUT 3', 'susinky vod klavesnic', '${TEXT2}', 'Spajdrmen', 'Lochotin', 0, 500, 600, '#ffac5e', '${TEXT1}', 3)" $DATABASE
 
