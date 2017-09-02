7d18cb7c0d28944a48cb5f1402192dd02c356260
root <root@tester.gc-system.cz>
2017-09-02 14:31:36 +0200

    database backup script

--- bin/backup_db.sh
+++ bin/backup_db.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-php bin/console doctrine:schema:drop --force
-php bin/console doctrine:schema:create
-
 export DATABASE="$(cat app/config/parameters.yml | grep database_name | awk '{print $2}')"
 export DPASSWORD="$(cat app/config/parameters.yml | grep database_password | awk -F"'" '{print $2}')"
 export MYSQL="mysql -u $DATABASE -p$DPASSWORD"
 
-sh src/StagBundle/bin/make_db.sh
-sh src/GuserBundle/bin/make_db.sh
-
+mysqldump -u$DATABASE -p$DPASSWORD $DATABASE --all-tablespaces --skip-lock-tables > database-backup-$(date +%Y%m%d%H%M%S).sql
