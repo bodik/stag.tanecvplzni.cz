f08b89d031a99770896f0ee39c484a9f143b2559
root <root@tester.gc-system.cz>
2017-09-02 12:01:42 +0200

    dep/prod database bootstrap tuning

--- bin/make_db.sh
+++ bin/make_db.sh
@@ -3,7 +3,7 @@ php bin/console doctrine:schema:create
 
 export DATABASE="$(cat app/config/parameters.yml | grep database_name | awk '{print $2}')"
 export DPASSWORD="$(cat app/config/parameters.yml | grep database_password | awk -F"'" '{print $2}')"
-export MYSQL="mysql -h 172.17.0.3 -u $DATABASE -p$DPASSWORD"
+export MYSQL="mysql -u $DATABASE -p$DPASSWORD"
 
 sh src/StagBundle/bin/make_db.sh
 sh src/GuserBundle/bin/make_db.sh
